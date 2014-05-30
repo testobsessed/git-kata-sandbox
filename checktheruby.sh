fecba1b36e6dd42f2f1025c14f3f34dec28a9a32
Elisabeth Hendrickson <ehendrickson@pivotallabs.com>
2014-05-30 09:45:14 -0700

    Adding script to make gocd work

---
+++ checktheruby.sh
@@ -0,0 +1,12 @@
+echo "RUBY VERSION FILE"
+catver=`cat .ruby-version | sed s/-//g`
+echo $catver
+echo "RUBY VERSION EXEC"
+rubyver=`ruby --version | awk '{ print $2 }'`
+echo $rubyver
+
+if [ $catver == $rubyver ]; then
+	exit 0
+else
+	exit 1
+fi
